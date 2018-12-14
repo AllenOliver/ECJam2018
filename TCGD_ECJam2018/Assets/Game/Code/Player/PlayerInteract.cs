643571a885d20285111047d0e33f58884c1a8550
Allen Oliver <32452527+AllenOliver@users.noreply.github.com>
2018-12-14 00:04:01 -0500

    Added interact script
    Needs to be expanded

--- TCGD_ECJam2018/Assets/Game/Code/Player/PlayerInteract.cs
+++ TCGD_ECJam2018/Assets/Game/Code/Player/PlayerInteract.cs
@@ -1,11 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 public class PlayerInteract : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider col)
+    public int RayLength;
+    private RaycastHit ray;
+
+    #region TESTING
+
+    public Text ui;
+    #endregion
+    void Update()
     {
-        //TODO Set a prompt to interact
+        Interact();
     }
+
+    void Interact()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * RayLength, Color.red, .5f);
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out ray, RayLength))
+        {
+            if (ray.collider.tag == "Interactable")
+            {
+                //TODO UI updates
+                ui.text = "Hit!";
+
+            }
+            else
+            {
+                ui.text = "";
+            }
+        }
+    }
+
+
 }
