f7990f0bff870839aa5a478d37a63bbbf7b2d188
Allen Oliver <32452527+AllenOliver@users.noreply.github.com>
2018-12-17 23:27:03 -0500

    Added Pause and some other logic

---
+++ TCGD_ECJam2018/Assets/Game/Code/Interactables/PC/PCInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCInteractable : BaseInteractable
+{
+    public override void SetGlobal()
+    {
+        Globals.PCFound = true;
+    }
+}
---
+++ TCGD_ECJam2018/Assets/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    private bool Paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Paused = false;
+        PauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!Paused)
+            {
+                PauseMenu.SetActive(true);
+                Paused = true;
+            }
+            else
+            {
+                PauseMenu.SetActive(false);
+                Paused = false;
+            }
+        }
+
+        if (PauseMenu.activeInHierarchy)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
